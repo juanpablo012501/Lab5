38d40680b792946ee77ebdae690b9009012b8e21
Juan Pablo Vélez M. <juanvelezm01@gmail.com>
2025-11-06 23:26:35 -0500

    Ciclo 0 terminado

--- src/presentation/SlowTetrisGUI.java
+++ src/presentation/SlowTetrisGUI.java
@@ -1,13 +1,15 @@
 package presentation;
 
 import javax.swing.*;
-import javax.awt.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class SlowTetrisGUI extends JFrame{
 	//Ciclo 0
 	public SlowTetrisGUI(){
 		super("SlowTetris");
 		prepareElements();
+		prepareActions();
 	}
 	
 	/**
@@ -30,5 +32,35 @@ public class SlowTetrisGUI extends JFrame{
 		setSize(width, height);
 		//ubicar
 		setLocationRelativeTo(null);
+		//Ajustar el cierre para poder aplicar la ventana de confirmación
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+	}
+	
+	/**
+	* Prepares the listener and the actions related to them
+	*/
+	private void prepareActions(){
+		//Oyente para confirmar cierre
+		addWindowListener(new WindowAdapter(){
+			@Override
+			public void windowClosing(WindowEvent e){
+				exit();
+			}
+		});
+	}
+	
+	/**
+	* Shows the confirmation window to exit
+	*/
+	private void exit(){
+		int salir = JOptionPane.showConfirmDialog(
+			this,
+			"¿Quiere salir de la app?",
+			"Confirmar salida",
+			JOptionPane.YES_NO_OPTION
+		);
+		if(salir == JOptionPane.YES_OPTION){
+			System.exit(0);
+		}
 	}
 }
